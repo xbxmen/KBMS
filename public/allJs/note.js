2be7150dc4991c74f8638b538601cbe3018a076d
FebV <cn.zhaoyifan@outlook.com>
2016-06-14 01:04:13 +0800

    now we can delete notebook

--- public/allJs/note.js
+++ public/allJs/note.js
@@ -1,3 +1,57 @@
-angular.module('noteApp', []).controller('noteBookController', function($scope){
-    $scope.data = 'haha';
-});
+var baseUrl = 'http://localhost/KBMS/public/note/i/'
+
+angular.module('noteApp', []).controller('noteBookController', ['$scope', '$http', '$rootScope', function noteBookController($scope, $http, $rootScope){
+    $rootScope.curNoteBookId = 1;
+    
+    $scope.getNoteBook = function(){
+        $http.get(baseUrl+'folder').then(function(response){
+        $rootScope.noteBooks = response.data;
+        });
+    }
+
+    $scope.changeNoteBookId = function(id){
+        $rootScope.curNoteBookId = id;
+    }
+
+    $scope.modifyNoteBookName = function(name){
+        $http.post(baseUrl+'folder/'+$rootScope.curNoteBookId, {'opt': 'mdf', 'folname': name}).then(function(response){
+            if(response.data == 0)
+                $scope. getNoteBook();
+            else
+                alert(response.data);
+        });
+    }
+
+    $scope.deleteNoteBook = function(){
+        $http.post(baseUrl+'folder/'+$rootScope.curNoteBookId, {'opt': 'del'}).then(function(response){
+            if(response.data == 0)
+                $scope. getNoteBook();
+            else
+                alert(response.data);
+        });
+    }
+
+
+    $scope.initNoteBookMenuClick = function(){
+    function ppp_click(event){
+        var u_id = null;
+        l_id = angular.element(this).attr("id");
+        u_id = l_id;
+        u_id = u_id.substr(1);
+        //console.log(event.clientX);
+        var x = event.clientX;
+        var y = event.clientY;
+        console.log(-y+120+" "+y);
+        angular.element(".more_N").css("top",(y-600)+"px");
+        angular.element(".more_N").show();
+
+    };
+    angular.element(".ppp_icon").click(ppp_click);
+    }
+
+    $scope.init = function(){
+        $scope.getNoteBook();
+    }
+
+    $scope.init();
+}]);
